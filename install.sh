919ced37b63378db53035386f24e213413d1e841
Kodie <kodiegoodwin@gmail.com>
2022-06-18 14:56:55 -0700

    yeetstate

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 cd $HOME
 sh -c "$(curl -fsLS chezmoi.io/get)"
 ~/bin/chezmoi --force init --apply https://github.com/enkoder/dotz.git
+~/bin/chezmoi state delete-bucket --bucket=scriptState
 ~/bin/chezmoi --force update
