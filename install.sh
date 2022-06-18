e128c481a77c936b95ba51c593a14d854c81aae1
enkoder <kodiegoodwin@gmail.com>
2022-06-18 19:06:45 +0000

    force init

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 cd $HOME
 sh -c "$(curl -fsLS chezmoi.io/get)"
-~/bin/chezmoi init --apply https://github.com/enkoder/dotz.git
+~/bin/chezmoi --force init --apply https://github.com/enkoder/dotz.git
